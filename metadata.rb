4c29abf0b98c0bfa0f6e4be1f5f6d2e54534c198
Fraser Scott <admin@privacythroughchoice.org>
2014-04-14 15:41:07 +0100

    content

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,9 @@ maintainer_email 'admin@privacythroughchoice.org'
 license          'All rights reserved'
 description      'Installs/Configures chef_app_screenshot'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
+depends 'yum', '~> 3.2'
+depends 'apt', '~> 2.3'
+depends 'phantomjs', '~> 1.0'
+depends 'git', '~> 4.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,40 @@
 # 
 # All rights reserved - Do Not Redistribute
 #
+case node.platform_family
+when "debian"
+  include_recipe 'apt'
+  package "imagemagick"
+when "rhel"
+  include_recipe 'yum'
+  package "ImageMagick"
+end
+
+include_recipe 'phantomjs'
+include_recipe 'git'
+
+
+ohai 'reload_passwd' do
+  action :nothing
+  plugin 'passwd'
+end
+
+user 'screenshot' do
+  system true
+  notifies :reload, 'ohai[reload_passwd]', :immediately
+end
+
+directory '/opt/screenshot' do
+  user 'screenshot'
+  group 'screenshot'
+  mode 0755
+end
+
+git '/opt/screenshot/app' do
+  repository 'https://github.com/PrivacyThroughChoice/app_screenshot'
+  revision 'master'
+  action :sync
+  user 'screenshot'
+  group 'screenshot'
+end
+
